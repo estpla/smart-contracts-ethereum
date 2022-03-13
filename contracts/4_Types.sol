46b68b3f1e4e98366645c949c66cbd7a4a56fc4b
Esteban Platero Horcajadas <eplatero@teralco.com>
2022-03-13 10:37:26 +0100

    Types and Enums

---
+++ contracts/4_Types.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Types {
+  string public myString = "myValue";
+  bool public myBool = true;
+  int public myInt = -1;
+  uint public myUint = 1;
+  uint8 public myUint8 = 8;
+  uint256 public myUint256 = 9999;
+}
